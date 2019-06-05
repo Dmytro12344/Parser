f5f2e05916a3448a5f4fefad587d4eef5105d912
Dmytro12344 <dmytro.vasiuk@nure.ua>
2019-06-06 01:05:55 +0300

    refactored WrapPars

--- web/Wraps/WrapPars.php
+++ web/Wraps/WrapPars.php
@@ -13,95 +13,57 @@ class WrapPars
         $guzzle = new GuzzleWrap();
         $linksMas = $this->globalMas();
         $countLinks = count($linksMas);
-        $str =[];
+
 
         $fp = fopen('parsed.csv', 'w');
 
 
 
-        for($i = 1; $i <= $countLinks; $i++) {
-            for ($j = 1; $j <= 500; $j++) {
+        for($i = 1; $i <= $countLinks; $i++)
+        {
+            for ($j = 1; $j <= 500; $j++)
+            {
                 $crawler = new Crawler($guzzle->getContent($this->linkPars($linksMas[$i], $j)));
-                for ($k = 0; $k < 20; $k++) {
+
+                for ($k = 0; $k < 20; $k++)
+                {
                     $filter = $crawler->filter('ul>li>.result')->eq($k);
+                    $str =[];
 
-
-                    if($filter->filterXPath("//*[@itemprop='name']")->count() <= 0)
+                    if ($filter->filterXPath("//*[@itemprop='name']")->count() <= 0)
                     {
                         continue 3;
                     }
-                    $companyName = $filter->filterXPath("//*[@itemprop='name']")->text();
-
-
-
-
-                    //check to correct telephone
-                    if ($filter->filterXPath("//*[@itemprop='telephone']")->count() > 0) {
-                        $companyTelephone = substr(preg_replace("/[^0-9]+/", "", $filter->filterXPath("//*[@itemprop='telephone']")->text()), 1);
-
-                    } else {
-                        continue;
-                    }
-
-                    if ($filter->filterXPath("//*[@itemprop='address']")->count() > 0) {
-                        $fullAddress = $filter->filterXPath("//*[@itemprop='address']")->text();
-
-
-                        //check to correct POSTAL COD
-                        if (!empty(strpos($fullAddress, 'Cod Postal'))) {
-                            $postal = explode(" ", strrchr($fullAddress, 'Postal'));
-                            if (isset($postal[1]) && substr($postal[1], -1) === ',') {
-                                $result = rtrim($postal[1], ',');
-                                $companyPostalCod = $result;
-                            } else {
-                                $companyPostalCod = '';
-                            }
-                        } else {
-                            $companyPostalCod = '';
-                        }
-
-                        //check to correct CIty
-
-                        if (!is_numeric(strrchr($fullAddress, ' '))) {
-                            $companyCity = strrchr($fullAddress, ' ');
-                        } else {
-                            $result = $this->before(strrchr($fullAddress, ', Cod Postal '), $fullAddress);
-                            $companyCity = strrchr($result, ' ');
-                        }
-
-                        //check to correct Address
-                        if (!ctype_upper(strrchr($fullAddress, ' '))) {
-                            if (!empty(strrchr($fullAddress, 'Jud. '))) {
-                                $companyAddress = $this->before(strrchr($fullAddress, ', Jud. '), $fullAddress);
-                            } elseif (!empty(strrchr($fullAddress, 'Cod Postal '))) {
-                                $companyAddress = $this->before(strrchr($fullAddress, ', Cod Postal '), $fullAddress);
-                            } else {
-                                $companyAddress = $this->before(strrchr($fullAddress, ', '), $fullAddress);
-                            }
-                            if (!empty(strrchr($companyAddress, 'Cod Postal '))) {
-                                $companyAddress = $this->before(strrchr($companyAddress, ', Cod '), $companyAddress);
-                            }
-                            if (!empty(strrchr($companyAddress, $companyCity))) {
-                                $companyAddress = $companyAddress = $this->before(strrchr($companyAddress, ", $companyCity"), $companyAddress);
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else {
-                        continue;
-                    }
-
-
-                    $str[] = [trim($companyName), trim($companyAddress), trim($companyCity), trim($companyPostalCod),trim($companyTelephone)];
-
-                    foreach($str as $value)
+                    if ($filter->filterXPath("//*[@itemprop='telephone']")->count() <= 0)
                     {
+                        continue;
+                    }
+                    if ($filter->filterXPath("//*[@itemprop='address']")->count() <= 0)
+                    {
+                        continue;
+                    }
+
+                    $companyName = $filter->filterXPath("//*[@itemprop='name']")->text();
+                    $companyTelephone = substr(preg_replace("/[^0-9]+/", "", $filter->filterXPath("//*[@itemprop='telephone']")->text()), 1);
+                    $fullAddress = $filter->filterXPath("//*[@itemprop='address']")->text();
+                    $companyPostalCod = $this->getPostalCod($fullAddress);
+                    $companyCity = $this->getCity($fullAddress);
+                    $companyAddress = $this->getAddress($fullAddress);
+
+
+
+
+
+                    $str[] = [trim($companyName), trim($companyAddress), trim($companyCity), trim($companyPostalCod), trim($companyTelephone)];
+
+                    foreach ($str as $value){
                         fputcsv($fp, $value);
                     }
                     var_dump($str);
 
-                }
+
                 echo "\n";
+            }
             }
         }
         fclose($fp);
@@ -142,5 +104,51 @@ class WrapPars
             return $str;
         }
         return '';
+    }
+
+    public function getPostalCod($address) : string
+    {
+        if (!empty(strpos($address, 'Cod Postal'))) {
+            $postal = explode(" ", strrchr($address, 'Postal'));
+            if (isset($postal[1]) && substr($postal[1], -1) === ',') {
+                $result = rtrim($postal[1], ',');
+                return $PostalCod = $result;
+            } else {
+                return $PostalCod = '';
+            }
+        } else {
+            return $PostalCod = '';
+        }
+    }
+
+    public function getCity($address) : string
+    {
+        if (!is_numeric(strrchr($address, ' ')))
+        {
+            return $City = strrchr($address, ' ');
+        } else {
+            $result = $this->before(strrchr($address, ', Cod Postal '), $address);
+            return $City = strrchr($result, ' ');
+        }
+    }
+
+    public function getAddress($fullAddress) : string
+    {
+            if (!empty(strrchr($fullAddress, 'Jud. '))) {
+                $companyAddress = $this->before(strrchr($fullAddress, ', Jud. '), $fullAddress);
+            } elseif (!empty(strrchr($fullAddress, 'Cod Postal '))) {
+                $companyAddress = $this->before(strrchr($fullAddress, ', Cod Postal '), $fullAddress);
+            } else {
+                $companyAddress = $this->before(strrchr($fullAddress, ', '), $fullAddress);
+            }
+            if (!empty(strrchr($companyAddress, 'Cod Postal '))) {
+                $companyAddress = $this->before(strrchr($companyAddress, ', Cod Postal'), $companyAddress);
+            }
+            if (!empty(strrchr($companyAddress, $this->getCity($fullAddress)))) {
+                $companyCity = $this->getCity($fullAddress);
+                $companyAddress = $this->before(strrchr($companyAddress, ", $companyCity"), $companyAddress);
+            }
+
+        return $companyAddress;
     }
 }
